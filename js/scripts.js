059fb522c742b234b5fc9300d415297bb845ee41
JebacKoda <55604333+JebacKoda@users.noreply.github.com>
2023-09-13 01:41:08 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,19 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '.navbar'
+  })
+  window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        var image = document.getElementById("indicator");
+        var opacity = 0;
+        image.style.opacity = opacity;
+    }else{
+        var image = document.getElementById("indicator");
+        var opacity = 1;
+        image.style.opacity = opacity;
+    }
+};
+document.getElementById("topw").style.fontWeight = "900";
+document.getElementById("topw").style.textDecorationLine = "underline";
